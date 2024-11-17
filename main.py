ec56e185d0bfef5a3901d9637f329d0a7fa3b077
GhostNikolay <snl-10@mail.ru>
2024-11-17 10:35:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+"""Создайте пользовательский класс исключения StepValueError, который наследуется от ValueError.
+Наследования достаточно, класс оставьте пустым при помощи оператора pass."""
+from turtledemo.penrose import start
+
+
+class StepValueError(ValueError):
+    pass
+
+
+"""Создайте класс Iterator, который обладает следующими свойствами:
+Атрибуты объекта:"""
+
+
+class Iterator:
+
+    # Методы:
+    # __init__(self, start, stop, step=1) - принимающий значения старта и конца итерации, а также шага.
+    # В этом методе в первую очередь проверяется step на равенство 0.
+    # Если равно, то выбрасывается исключение StepValueError('шаг не может быть равен 0')"""
+
+    def __init__(self, start, stop,step=1):  # метод, сбрасывающий значение pointer на start и возвращающий сам объект итератора.
+        self.start = start  # целое число, с которого начинается итерация.
+        self.stop = stop  # целое число, на котором заканчивается итерация.
+        self.step = step  # шаг, с которым совершается итерация.
+        self.pointer = start  # pointer - указывает на текущее число в итерации (изначально start)
+
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if (self.step > 0 and self.pointer > self.stop)  or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration
+        current = self.pointer
+        self.pointer += self.step
+        return current
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
